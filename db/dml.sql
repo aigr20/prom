c16499fe9080f91bee948209c169d0b288d17115
Wilhelm Gren <wilhelmgren@outlook.com>
2023-07-06 14:38:54 +0200

    Prepare database for statuses

--- db/dml.sql
+++ db/dml.sql
@@ -1,4 +1,5 @@
 DELETE FROM issues;
+DELETE FROM issue_statuses;
 DELETE FROM projects;
 DELETE FROM users;
 
@@ -8,12 +9,19 @@ INSERT INTO projects (project_id, project_name, creation_date, last_changed) VAL
   (3, "Mina Recept", "2023-06-25 15:22:35", "2023-06-26 12:00:14")
 ;
 
-INSERT INTO issues (issue_id, issue_title, issue_description, creation_date, last_changed, project) VALUES
-  (1, "Make accessible", "Appen måste gå att använda av alla!", "2023-06-28 14:00:00", "2023-06-28 15:15:00", 1),
-  (2, "Make more fun", "Lägg in mycket färger, appen skall vara rolig!", "2023-06-28 14:03:00", "2023-06-28 15:20:00", 1),
-  (3, "Update damage system", "Damage system must be more complicated", "2023-06-29 13:12:03", "2023-06-29 16:12:00", 2),
-  (4, "Receptmodell", "", "2023-06-30 10:00:00", "2023-06-30 15:00:34", 3)
+INSERT INTO issue_statuses (status_id, status_text) VALUES
+ (1, "TODO"),
+ (2, "In Progress"),
+ (3, "Finished")
 ;
+
+INSERT INTO issues (issue_id, issue_title, issue_description, creation_date, last_changed, project, issue_status) VALUES
+  (1, "Make accessible", "Appen måste gå att använda av alla!", "2023-06-28 14:00:00", "2023-06-28 15:15:00", 1, 1),
+  (2, "Make more fun", "Lägg in mycket färger, appen skall vara rolig!", "2023-06-28 14:03:00", "2023-06-28 15:20:00", 1, 2),
+  (3, "Update damage system", "Damage system must be more complicated", "2023-06-29 13:12:03", "2023-06-29 16:12:00", 2, 3),
+  (4, "Receptmodell", "", "2023-06-30 10:00:00", "2023-06-30 15:00:34", 3, 1)
+;
+
 
 INSERT INTO users (forename, surname, user_email, user_password) VALUES
   ("Test", "Testson", "testis@test.com", "gargamel")
