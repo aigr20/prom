057fa4ef5cbd5003cb6aaa46a8cc4ab2b37be2e9
Wilhelm Gren <wilhelmgren@outlook.com>
2023-07-06 15:24:37 +0200

    Create helpers for scanning into model structs

--- server/models/issue.go
+++ server/models/issue.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"log"
+	"time"
+)
 
 type Issue struct {
 	ID          int       `json:"id"`
@@ -9,6 +13,33 @@ type Issue struct {
 	Created     time.Time `json:"createdAt"`
 	Updated     time.Time `json:"updatedAt"`
 	ProjectID   int       `json:"-"`
+	Status      string    `json:"status"`
+}
+
+func ScanIssues(rows *sql.Rows) ([]Issue, error) {
+	issues := make([]Issue, 0)
+	for rows.Next() {
+		var issue Issue
+		err := rows.Scan(&issue.ID, &issue.Title, &issue.Description, &issue.Created, &issue.Updated, &issue.ProjectID, &issue.Status)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		issues = append(issues, issue)
+	}
+	return issues, nil
+}
+
+func ScanIssue(row *sql.Row) (Issue, error) {
+	if err := row.Err(); err != nil {
+		return Issue{}, err
+	}
+	var issue Issue
+	err := row.Scan(&issue.ID, &issue.Title, &issue.Description, &issue.Created, &issue.Updated, &issue.ProjectID, &issue.Status)
+	if err != nil {
+		return Issue{}, err
+	}
+	return issue, nil
 }
 
 type IssueCreateForm struct {
