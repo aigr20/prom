96f7d4b3d24fb654891fdc2e4cebbbae26adb372
Wilhelm Gren <wilhelmgren@outlook.com>
2023-08-17 15:43:34 +0200

    Start work on tag creation

--- client/src/components/ProjectSettings/ProjectSettings.tsx
+++ client/src/components/ProjectSettings/ProjectSettings.tsx
@@ -1,3 +1,32 @@
+import { useOutletContext } from "react-router-dom";
+import type { IProjectViewOutletContext } from "../../types/project";
+import "./ProjectSettings.css";
+
 export default function ProjectSettings() {
-  return <></>;
+  const { project } = useOutletContext<IProjectViewOutletContext>();
+
+  return (
+    <>
+      <h2>Project Settings</h2>
+      <section>
+        <h3>Tags</h3>
+        <ul className="settings--tag_list-wrapper">
+          {project.tags.map((tag) => {
+            return (
+              <li
+                key={`${tag.id}-${tag.text}`}
+                className="tag"
+                style={{ background: tag.color }}
+              >
+                {tag.text}
+              </li>
+            );
+          })}
+        </ul>
+        <form className="settings-create_tag-wrapper">
+          <input type="text" maxLength={15} placeholder="Tag label" />
+        </form>
+      </section>
+    </>
+  );
 }
