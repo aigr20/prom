4ab802af46dca7000e9c7486f7f37afcb944c887
Wilhelm Gren <wilhelmgren@outlook.com>
2023-08-20 17:16:23 +0200

    Move logic into Tag Creator hook

--- client/src/components/ProjectSettings/ProjectSettings.tsx
+++ client/src/components/ProjectSettings/ProjectSettings.tsx
@@ -1,26 +1,12 @@
-import { useEffect, useState } from "react";
-import { useOutletContext, useRevalidator } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 import { useTagCreation } from "../../hooks/projectSettingsHooks";
-import { createTag, getProjectTagCounts } from "../../services/projects";
-import type {
-  IProjectViewOutletContext,
-  ITag,
-  ITagCount,
-} from "../../types/project";
+import type { IProjectViewOutletContext } from "../../types/project";
 import ColorPicker from "../util/ColorPicker/ColorPicker";
 import "./ProjectSettings.css";
 
 export default function ProjectSettings() {
   const { project } = useOutletContext<IProjectViewOutletContext>();
-  const [tags, setTags] = useState<ITag[]>(project.tags);
-  const [tagCounts, setTagCounts] = useState<ITagCount[]>([]);
-  const tagCreator = useTagCreation();
-  const { revalidate } = useRevalidator();
-  useEffect(() => {
-    getProjectTagCounts({ projectId: project.id }).then(({ data }) => {
-      setTagCounts([...data]);
-    });
-  }, [project.id]);
+  const tagCreator = useTagCreation(project);
 
   return (
     <>
@@ -28,7 +14,7 @@ export default function ProjectSettings() {
       <section>
         <h3>Tags</h3>
         <ul className="settings--tag_list-wrapper">
-          {tags.map((tag) => {
+          {tagCreator.tags.map((tag) => {
             return (
               <li
                 key={`${tag.id}-${tag.text}`}
@@ -36,25 +22,16 @@ export default function ProjectSettings() {
                 style={{ background: tag.color }}
               >
                 {tag.text} (
-                {tagCounts.find((t) => t.tag === tag.text)?.count ?? 0})
+                {tagCreator.tagCounts.find((t) => t.tag === tag.text)?.count ??
+                  0}
+                )
               </li>
             );
           })}
         </ul>
         <form
           className="settings-create_tag-wrapper"
-          onSubmit={(e) => {
-            e.preventDefault();
-            createTag({
-              projectId: project.id,
-              tagText: tagCreator.tagText,
-              tagColor: tagCreator.tagColor,
-            }).then(({ data }) => {
-              if (!data) return;
-              setTags((prev) => [...prev, data]);
-              revalidate();
-            });
-          }}
+          onSubmit={tagCreator.createTagCB}
         >
           <label htmlFor="tag-label">Tag name</label>
           <input
